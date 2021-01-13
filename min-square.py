6638ea9c9bac3b4e934dfbee7071cd7d5fca1683
Eunki <ppyapmail@gmail.com>
2021-01-13 23:34:27 +0900

    Min square linear regression with data and env file

--- min-square.py
+++ min-square.py
@@ -1,6 +1,7 @@
 import random
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 
 
 def linear_regression(x_list, y_list):
@@ -29,15 +30,30 @@ def linear_regression(x_list, y_list):
     return (m, b)
 
 
-N = 5
+def read_weather_history():
+    # "Temperature (C)": x
+    # "Wind Speed (km/h)": y
+    frame = pd.read_csv("weatherHistory.csv")
+    x_data = frame["Temperature (C)"]
+    y_data = frame["Wind Speed (km/h)"]
+    return (x_data, y_data)
+
+
 # x_coord = [n*2 for n in range(N)]
 # y_coord = [random.randint(1, 10) for n in range(N)]
+x_coord, y_coord = read_weather_history()
+assert len(x_coord) == len(y_coord)
+N = len(x_coord)
 m, b = linear_regression(x_coord, y_coord)
 y_pred = [m*x_coord[i]+b for i in range(N)]
 
 error = sum((y_pred[i] - y_coord[i]) ** 2 for i in range(N))
-print("Dataset:", list(zip(x_coord, y_coord)))
+# print("Dataset:", list(zip(x_coord, y_coord)))
 print("Linear Regression:", "%fx + %f" % (m, b))
 print("Error:", error)
-plt.scatter(x_coord, y_coord)
-plt.plot(x_coord, y_pred)
+plt.scatter(x_coord, y_coord, s=1, alpha=0.1)
+plt.plot(x_coord, y_pred, color='red')
+plt.xlabel("Temperature (C)")
+plt.ylabel("Wind Speed (km/h)")
+plt.title("Linear Regression: Temperature vs. Wind Speed")
+plt.show()
